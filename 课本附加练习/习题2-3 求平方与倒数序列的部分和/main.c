97e259036a70dd0b709718d7a32cfe36a3f07de3
Myth Li <lxbpxylps@outlook.com>
2019-09-26 20:59:14 +0800

    update 2019/9/26

---
+++ 课本附加练习/习题2-3 求平方与倒数序列的部分和/main.c
@@ -0,0 +1,26 @@
+﻿#define _CRT_SECURE_NO_WARNINGS
+
+#include <stdio.h>
+#include <math.h>
+
+double calc(int x); /*计算m^​2​​+1/m*/
+
+int main(void)
+{
+	int m, n;
+	double sum = 0.0;
+
+	scanf("%d %d", &m, &n);
+	for (int num = m; num <= n; num++)
+		sum = sum + calc(num);
+	printf("sum = %.6f", sum);
+
+	return 0;
+}
+
+double calc(int x)
+{
+	double result;
+	result = pow((double)x, 2.0) + 1.0 / (double)x;
+	return result;
+}
