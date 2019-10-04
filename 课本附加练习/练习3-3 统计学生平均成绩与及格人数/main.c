f966d4bae109761943dab97c0a952356003a5fa1
Myth Li <lxbpxylps@outlook.com>
2019-10-04 18:40:25 +0800

    update 2019/10/4

---
+++ 课本附加练习/练习3-3 统计学生平均成绩与及格人数/main.c
@@ -0,0 +1,32 @@
+#define _CRT_SECURE_NO_WARNINGS
+
+#include <stdio.h>
+
+int main(void)
+{
+	int N = 0, count = 0, score = 0, sum = 0;
+	float average = 0.0;
+
+	scanf("%d", &N);
+	if (N == 0)
+	{
+		printf("average = 0.0\n");
+		printf("count = 0");
+	}
+	else
+	{
+		int i;
+		for (i = 1; i <= N; i++)
+		{
+			scanf("%d", &score);
+			if (score >= 60)
+				count++;
+			sum = sum + score;
+		}
+		average = (float)sum / (float)N;
+		printf("average = %.1f\n", average);
+		printf("count = %d", count);
+	}
+
+	return 0;
+}
